d33ad6f08121d0f0c62f72bb7196c637ca5a2d71
Feast <feast107@gmail.com>
2023-11-05 01:42:14 +0800

    Update document

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Antelcat.Avalonia.I18N
- Reactive language support for Avalonia applications when using .resx file.
+<div align="center">
+
+<h1>
+
+Antelcat.Avalonia.`{I18N}`
+
+</h1>
+
+给Avalonia应用程序提供响应式的多语言支持。
+
+</div>
+
+<p align="center">
+    <img alt="dotnet-version" src="https://img.shields.io/badge/Avalonia-%3E%3D11.0.0-AE42F8.svg"/>
+    <img alt="csharp-version" src="https://img.shields.io/badge/C%23-preview-3BA93F.svg"/>
+    <img alt="nuget" src="https://img.shields.io/badge/Nuget-v1.0.0-blue.svg"/>
+</p>
+
+---
+
+## 📖 About
+
+本项目构建自 [Antelcat.Wpf.I18N](https://github.com/Antelcat/Antelcat.Avalonia.I18N) ，并且有着完全一致的API。
--- src/Antelcat.Avalonia.I18N.Demo/App.axaml.cs
+++ src/Antelcat.Avalonia.I18N.Demo/App.axaml.cs
@@ -1,8 +1,10 @@
 using Antelcat.Avalonia.I18N.Demo.ViewModels;
 using Antelcat.Avalonia.I18N.Demo.Views;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
 
 namespace Antelcat.Avalonia.I18N.Demo
 {
@@ -11,6 +13,10 @@ namespace Antelcat.Avalonia.I18N.Demo
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
+            if (Design.IsDesignMode)
+            {
+                RequestedThemeVariant = ThemeVariant.Dark;
+            }
         }
 
         public override void OnFrameworkInitializationCompleted()
